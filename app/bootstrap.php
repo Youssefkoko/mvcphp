477580f6aee6705316a5e1911869ae31854858de
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-01 16:39:06 +0100

    create spl load function to require all files automatically

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
+// lOAD CONFIG 
+require_once 'config/config.php';
 // Load libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+
+// Auto LOad Core Libraries 
+spl_autoload_register(function($className){
+  require_once 'libraries/'. $className .'.php';
+});
